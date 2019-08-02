ef69937d3a682fd071efd6b8f22de424da8165c4
huxiaoningsworld <huxiaoningsworld@sina.com>
2019-08-02 20:30:04 +0800

    netty 5.1 DelimiterBasedFrameDecoder 分隔符解码器 应用开发

---
+++ netty/src/main/java/org/aidan/chapter051/EchoServer.java
@@ -0,0 +1,70 @@
+package org.aidan.chapter051;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class EchoServer {
+
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            /**
+                             * 分隔符($_)消息解码器
+                             */
+                            ByteBuf delimiter = Unpooled.copiedBuffer("$_".getBytes());
+                            socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, delimiter));
+                            socketChannel.pipeline().addLast(new StringDecoder());
+                            socketChannel.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class EchoServerHandler extends ChannelHandlerAdapter {
+        private int counter = 0;
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            String body = (String) msg;
+            System.out.println("This is " + ++counter + " times receive client:[" + body + "]");
+            body += "$_";
+            ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+            ctx.writeAndFlush(echo);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+        new EchoServer().bind(port);
+    }
+}
---
+++ netty/src/main/java/org/aidan/chapter051/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * DelimiterBasedFrameDecoder 应用开发
+ * <p>
+ * EchoServer 接收到 EchoClient 的请求后将原始消息打印出来，并将原始消息返回给客户端。
+ */
+package org.aidan.chapter051;
